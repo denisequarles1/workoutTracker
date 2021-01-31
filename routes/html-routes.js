a620f32195d412859c828b7270b924ca5f6f5a1c
denisequarles1 <denise.quarles1@gmail.com>
2021-01-30 21:44:17 -0500

    updated html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = (app) => {
 //index
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '..public/index.html'));
 });
 //exercise
 app.get('/exercise', (req, res) => {
